976fa1ab96d253d9cb68a64236a9b4907843f977
NicolasTu <nicolastu98@hotmail.com>
2022-08-30 23:35:00 -0300

    cargando nuevo proyecto sencillo en react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import { useState } from 'react';
+import html2canvas from 'html2canvas';
 function App() {
+  // Linea 1 variable, setLinea1 va a modificar variable
+  const [linea1, setLinea1] = useState('');
+  const [linea2, setLinea2] = useState('');
+  const [imagen, setImagen] = useState('');
+
+  const onChangeLinea1 = function(evento){
+    setLinea1(evento.target.value)
+  }
+
+  const onChangeLinea2 = function(evento){
+    setLinea2(evento.target.value)
+  }
+
+  const onChangeImagen = function(evento){
+    setImagen(evento.target.value)
+  }
+
+  const onClickExportar = function(evento){
+    html2canvas(document.querySelector("#meme")).then(canvas => {
+      var img    = canvas.toDataURL("image/png");
+      var link = document.createElement('a');
+      link.download = 'meme.png';
+      link.href = img;
+      link.click();
+  });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select onChange={onChangeImagen}>
+        <option value="fire">fire</option>
+        <option value="futurama">futurama</option>
+        <option value="history">history</option>
+        <option value="matrix">matrix</option>
+      </select><br />
+      
+      <input onChange={onChangeLinea1} type="text" placeholder='Linea 1'/><br />
+      <input onChange={onChangeLinea2} type="text" placeholder='Linea 2'/><br />
+      <button onClick={onClickExportar}>Exportar</button>
+
+      <div className='meme' id='meme'>
+        <span>{linea1}</span><br />
+        <img src={"/img/"+ imagen +".jpg"} alt='' />
+        <span>{linea2}</span><br />
+      </div>
     </div>
   );
 }
